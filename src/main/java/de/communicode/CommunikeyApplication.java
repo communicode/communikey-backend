31a24541b1e7fd8a86f60f32e95a712de40768ce
Sven Greb <sgreb@communicode.de>
2016-09-05 15:40:30 +0200

    Implement communikey main application class

---
+++ src/main/java/de/communicode/CommunikeyApplication.java
@@ -0,0 +1,18 @@
+/*
+  * Copyright (C) 2016 communicode AG
+ */
+package de.communicode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Sven Greb &lt;sgreb@communicode.de&gt;
+ */
+@SpringBootApplication
+public class CommunikeyApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(CommunikeyApplication.class, args);
+  }
+}
