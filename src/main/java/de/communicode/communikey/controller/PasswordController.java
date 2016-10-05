4f2fb131ec18ec35762c92c3c816769a1f11272d
Sven Greb <sgreb@communicode.de>
2016-10-05 12:09:32 +0200

    CCKEY-23 Adapt code style to communicode standards

--- src/main/java/de/communicode/communikey/controller/PasswordController.java
+++ src/main/java/de/communicode/communikey/controller/PasswordController.java
@@ -25,100 +25,100 @@ import org.springframework.web.bind.annotation.PostMapping;
 @Controller
 public class PasswordController {
 
-  @Autowired
-  private PasswordRepository passwordRepository;
-  @Autowired
-  private PasswordService passwordService;
+    @Autowired
+    private PasswordRepository passwordRepository;
+    @Autowired
+    private PasswordService passwordService;
 
-  /**
-   * Endpoint to list all {@link Password} entities.
-   *
-   * @param model the model holding the attributes
-   * @return the string to the endpoint
-   */
-  @GetMapping(value = "/passwords")
-  String listPasswords(Model model) {
-    model.addAttribute("passwords", passwordRepository.findAll());
-    return "passwords";
-  }
+    /**
+     * Endpoint to list all {@link Password} entities.
+     *
+     * @param model the model holding the attributes
+     * @return the string to the endpoint
+     */
+    @GetMapping(value = "/passwords")
+    String listPasswords(Model model) {
+        model.addAttribute("passwords", passwordRepository.findAll());
+        return "passwords";
+    }
 
-  /**
-   * Endpoint to delete a {@link Password} entity.
-   *
-   * @param id the ID of the {@link Password} to delete
-   * @return the string to the endpoint redirection
-   */
-  @GetMapping(value = "passwords/{id}/delete")
-  String deletePassword(@PathVariable long id) {
-    passwordRepository.delete(id);
-    return "redirect:/passwords";
-  }
+    /**
+     * Endpoint to delete a {@link Password} entity.
+     *
+     * @param id the ID of the {@link Password} to delete
+     * @return the string to the endpoint redirection
+     */
+    @GetMapping(value = "passwords/{id}/delete")
+    String deletePassword(@PathVariable long id) {
+        passwordRepository.delete(id);
+        return "redirect:/passwords";
+    }
 
-  /**
-   * Endpoint to the form to edit a {@link Password} entity.
-   *
-   * <p>
-   *   This is used to process the data entered by the user into the form.
-   * </p>
-   *
-   * @param id the ID of the {@link Password} to edit
-   * @param model the model holding the attributes
-   * @return the string to the endpoint
-   */
-  @GetMapping(value = "/passwords/{id}/edit")
-  String editPasswordForm(@PathVariable long id, Model model) {
-    Password password = passwordRepository.findOne(id);
-    EditPasswordForm editPasswordForm = new EditPasswordForm();
-    editPasswordForm.setValue(password.getValue());
-    model.addAttribute("password", password);
-    model.addAttribute("editPasswordForm", editPasswordForm);
-    return "/passwords-edit";
-  }
+    /**
+     * Endpoint to the form to edit a {@link Password} entity.
+     *
+     * <p>
+     * This is used to process the data entered by the user into the form.
+     * </p>
+     *
+     * @param id    the ID of the {@link Password} to edit
+     * @param model the model holding the attributes
+     * @return the string to the endpoint
+     */
+    @GetMapping(value = "/passwords/{id}/edit")
+    String editPasswordForm(@PathVariable long id, Model model) {
+        Password password = passwordRepository.findOne(id);
+        EditPasswordForm editPasswordForm = new EditPasswordForm();
+        editPasswordForm.setValue(password.getValue());
+        model.addAttribute("password", password);
+        model.addAttribute("editPasswordForm", editPasswordForm);
+        return "/passwords-edit";
+    }
 
-  /**
-   * Endpoint to edit a {@link Password} entity.
-   *
-   * @param id the ID of the {@link Password} to edit
-   * @param editPasswordForm the {@link EditPasswordForm} as named {@link Model} attribute
-   * @return the string to the endpoint redirection
-   */
-  @PostMapping(value = "/passwords/{id}/edit")
-  String editPassword(@PathVariable long id, @ModelAttribute("editPasswordForm") EditPasswordForm editPasswordForm) {
-    Password password = passwordRepository.findOne(id);
-    password.setValue(editPasswordForm.getValue());
-    passwordService.modifyPasswordValue(passwordRepository.findOne(id), editPasswordForm.getValue());
-    passwordRepository.save(password);
-    return "redirect:/passwords";
-  }
+    /**
+     * Endpoint to edit a {@link Password} entity.
+     *
+     * @param id               the ID of the {@link Password} to edit
+     * @param editPasswordForm the {@link EditPasswordForm} as named {@link Model} attribute
+     * @return the string to the endpoint redirection
+     */
+    @PostMapping(value = "/passwords/{id}/edit")
+    String editPassword(@PathVariable long id, @ModelAttribute("editPasswordForm") EditPasswordForm editPasswordForm) {
+        Password password = passwordRepository.findOne(id);
+        password.setValue(editPasswordForm.getValue());
+        passwordService.modifyPasswordValue(passwordRepository.findOne(id), editPasswordForm.getValue());
+        passwordRepository.save(password);
+        return "redirect:/passwords";
+    }
 
-  /**
-   * Endpoint to the form to create a new {@link Password} entity.
-   *
-   * <p>
-   *   This is used to process the data entered by the user into the form.
-   * </p>
-   *
-   * @param newPassword the new {@link Password} entity
-   * @param model the {@link Model} holding the attributes
-   * @return the string to the endpoint
-   */
-  @GetMapping(value = "/passwords/new")
-  String newPasswordForm(Password newPassword, Model model) {
-    model.addAttribute("password", newPassword);
-    model.addAttribute("newPasswordForm", new NewPasswordForm());
-    return "/passwords-new";
-  }
+    /**
+     * Endpoint to the form to create a new {@link Password} entity.
+     *
+     * <p>
+     * This is used to process the data entered by the user into the form.
+     * </p>
+     *
+     * @param newPassword the new {@link Password} entity
+     * @param model       the {@link Model} holding the attributes
+     * @return the string to the endpoint
+     */
+    @GetMapping(value = "/passwords/new")
+    String newPasswordForm(Password newPassword, Model model) {
+        model.addAttribute("password", newPassword);
+        model.addAttribute("newPasswordForm", new NewPasswordForm());
+        return "/passwords-new";
+    }
 
-  /**
-   * Endpoint to create a new {@link Password} entity.
-   *
-   * @param newPasswordForm the {@link NewPasswordForm} as named {@link Model} attribute
-   * @return the string to the endpoint redirection
-   */
-  @PostMapping(value = "/passwords/new")
-  String newPassword(@ModelAttribute("newPasswordForm") NewPasswordForm newPasswordForm) {
-    Password password = new Password(newPasswordForm.getValue());
-    passwordRepository.save(password);
-    return "redirect:/passwords";
-  }
+    /**
+     * Endpoint to create a new {@link Password} entity.
+     *
+     * @param newPasswordForm the {@link NewPasswordForm} as named {@link Model} attribute
+     * @return the string to the endpoint redirection
+     */
+    @PostMapping(value = "/passwords/new")
+    String newPassword(@ModelAttribute("newPasswordForm") NewPasswordForm newPasswordForm) {
+        Password password = new Password(newPasswordForm.getValue());
+        passwordRepository.save(password);
+        return "redirect:/passwords";
+    }
 }
