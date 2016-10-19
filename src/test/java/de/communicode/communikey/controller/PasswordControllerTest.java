5a015852899b20202b13d975651525639b2d2a1c
Sven Greb <sgreb@communicode.de>
2016-10-19 14:03:59 +0200

    CCKEY-27 Implement CSRF handling in password controller unit tests
    Added the "spring-security-test" Maven dependency which provides static
    methods to run tests as a configured user and to handle CSRF attributes.

--- src/test/java/de/communicode/communikey/controller/PasswordControllerTest.java
+++ src/test/java/de/communicode/communikey/controller/PasswordControllerTest.java
@@ -6,13 +6,19 @@
 */
 package de.communicode.communikey.controller;
 
+import static de.communicode.communikey.CommunikeyConstants.ENDPOINT_PASSWORDS;
+import static de.communicode.communikey.CommunikeyConstants.ENDPOINT_ROOT;
+import static de.communicode.communikey.CommunikeyConstants.REQUEST_PASSWORD_NEW;
 import static de.communicode.communikey.CommunikeyConstants.TEMPLATE_PASSWORD_EDIT;
 import static de.communicode.communikey.CommunikeyConstants.TEMPLATE_PASSWORD_NEW;
+import static de.communicode.communikey.CommunikeyConstants.asRedirect;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.user;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 
 import de.communicode.communikey.CommunikeyApplication;
 import de.communicode.communikey.domain.Password;
@@ -27,6 +33,8 @@ import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
+
+import javax.servlet.Filter;
 
 /**
  * Unit tests for the {@link PasswordController} class.
@@ -43,31 +51,40 @@ public class PasswordControllerTest {
     private PasswordRepository passwordRepository;
     @Autowired
     private WebApplicationContext wac;
+    @Autowired
+    private Filter springSecurityFilterChain;
 
     @Before
     public void setup() {
         passwordController = new PasswordController();
-        mockMvc = MockMvcBuilders.webAppContextSetup(wac).build();
+        mockMvc = MockMvcBuilders.webAppContextSetup(wac)
+            .addFilters(springSecurityFilterChain)
+            .defaultRequest(get(ENDPOINT_ROOT).with(user("user").password("pass").roles("USER")))
+            .build();
     }
 
     @Test
     public void passwordsEndpoint() throws Exception {
-        mockMvc.perform(get("/passwords"))
+        mockMvc.perform(get(ENDPOINT_PASSWORDS))
             .andExpect(status().isOk())
-            .andExpect(view().name("passwords"));
+            .andExpect(view().name(ENDPOINT_PASSWORDS));
     }
 
     @Test
     public void passwordsNewEndpoints() throws Exception {
-        mockMvc.perform(get("/passwords/new"))
+        mockMvc.perform(get(REQUEST_PASSWORD_NEW)
+            .with(csrf())
+        )
             .andExpect(status().isOk())
             .andExpect(model().attributeExists("password"))
             .andExpect(model().attributeExists("newPasswordForm"))
             .andExpect(model().size(2))
             .andExpect(view().name(TEMPLATE_PASSWORD_NEW));
 
-        mockMvc.perform(post("/passwords/new"))
-            .andExpect(view().name("redirect:/passwords"))
+        mockMvc.perform(post(REQUEST_PASSWORD_NEW)
+            .with(csrf())
+        )
+            .andExpect(view().name(asRedirect(ENDPOINT_PASSWORDS)))
             .andExpect(status().is3xxRedirection());
     }
 
@@ -83,8 +100,10 @@ public class PasswordControllerTest {
             .andExpect(model().attributeExists("editPasswordForm"))
             .andExpect(model().size(2));
 
-        mockMvc.perform(post("/passwords/1/edit"))
-            .andExpect(view().name("redirect:/passwords"))
+        mockMvc.perform(post("/passwords/1/edit")
+            .with(csrf())
+        )
+            .andExpect(view().name(asRedirect(ENDPOINT_PASSWORDS)))
             .andExpect(status().is3xxRedirection());
     }
 
@@ -94,7 +113,7 @@ public class PasswordControllerTest {
         passwordRepository.save(password);
 
         mockMvc.perform(get("/passwords/1/delete"))
-            .andExpect(view().name("redirect:/passwords"))
+            .andExpect(view().name(asRedirect(ENDPOINT_PASSWORDS)))
             .andExpect(status().is3xxRedirection());
     }
 }
