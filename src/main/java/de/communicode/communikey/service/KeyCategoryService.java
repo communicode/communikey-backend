edea55778b9e5ca7153e9c10372ad046192ab7aa
Sven Greb <sgreb@communicode.de>
2016-11-10 15:55:42 +0100

    CCKEY-45 Update Key- and KeyCategory service interfaces

--- src/main/java/de/communicode/communikey/service/KeyCategoryService.java
+++ src/main/java/de/communicode/communikey/service/KeyCategoryService.java
@@ -9,33 +9,38 @@ package de.communicode.communikey.service;
 import de.communicode.communikey.domain.Key;
 import de.communicode.communikey.domain.KeyCategory;
 import de.communicode.communikey.domain.User;
+import de.communicode.communikey.exception.KeyCategoryNotFoundException;
+import de.communicode.communikey.exception.KeyNotFoundException;
+import de.communicode.communikey.exception.UserNotFoundException;
 import de.communicode.communikey.repository.KeyCategoryRepository;
 
 import java.util.Optional;
 import java.util.Set;
 
 /**
- * A service to interact with the {@link KeyCategoryRepository}.
+ * A service to process {@link KeyCategory} entities.
  *
  * @author sgreb@communicode.de
  * @since 0.2.0
  */
 public interface KeyCategoryService {
+
     /**
      * Deletes the given {@link KeyCategory}.
      *
      * <p>
-     *     <strong>This is a recursive action that deletes all {@link KeyCategory} entities where the given {@code category} is the parent of!</strong>
+     *     <strong>This is a recursive action that deletes all {@link Key} entities owned by this {@code category} and all {@link KeyCategory} entities where
+     *     the given {@code category} is the parent of!</strong>
      *
-     * @param category The {@link KeyCategory} to delete
-     * @throws NullPointerException if the given {@code category} is null
+     * @param category the {@link KeyCategory} to delete
+     * @throws NullPointerException if the given {@code category} entity is null
      */
     void delete(KeyCategory category) throws NullPointerException;
 
     /**
      * Gets all {@link KeyCategory} entities of the {@link KeyCategoryRepository}.
      *
-     * @return a collection of all {@link KeyCategory} entities
+     * @return a collection of all found {@link KeyCategory} entities
      */
     Set<KeyCategory> getAll();
 
@@ -43,25 +48,16 @@ public interface KeyCategoryService {
      * Gets all {@link KeyCategory} entities created by the given {@link User}.
      *
      * @param creator the {@link User} to get all {@link KeyCategory} entities of
-     * @return a collection of all {@link KeyCategory} entities
-     * @throws NullPointerException if the given {@code creator} is null
+     * @return a collection of all found {@link KeyCategory} entities
+     * @throws UserNotFoundException if the given {@code creator} has not been found
      */
-    Set<KeyCategory> getAllByCreator(User creator) throws NullPointerException;
-
-    /**
-     * Gets all {@link Key} entities of the given {@link KeyCategory}.
-     *
-     * @param category the {@link KeyCategory} to get all {@link Key} entities of
-     * @return a collection of all {@link KeyCategory} entities
-     * @throws NullPointerException if the given {@code category} is null
-     */
-    Set<Key> getAllKeys(KeyCategory category) throws NullPointerException;
+    Set<KeyCategory> getAllByCreator(User creator) throws UserNotFoundException;
 
     /**
      * Gets all {@link KeyCategory} entities with the given {@code name}.
      *
      * @param name the name of the {@link KeyCategory} entities to find
-     * @return a collection of all {@link KeyCategory} entities
+     * @return a collection of all found {@link KeyCategory} entities
      * @throws IllegalArgumentException if the given {@code name} is empty
      */
     Set<KeyCategory> getAllByName(String name) throws IllegalArgumentException;
@@ -69,37 +65,47 @@ public interface KeyCategoryService {
     /**
      * Gets all {@link KeyCategory} entities owned by the given parent {@link KeyCategory}.
      *
-     * @param category The parent {@link KeyCategory} to get all child entities of
-     * @return a collection of all {@link KeyCategory} entities
-     * @throws NullPointerException if the given {@code category} is null
+     * @param category the parent {@link KeyCategory} to get all child entities of
+     * @return a collection of all found {@link KeyCategory} entities
+     * @throws KeyCategoryNotFoundException if the given {@code category} has not been found
      */
-    Set<KeyCategory> getAllByParent(KeyCategory category) throws NullPointerException;
+    Set<KeyCategory> getAllByParent(KeyCategory category) throws KeyCategoryNotFoundException;
 
     /**
      * Gets all {@link KeyCategory} entities the given {@link User} is responsible for.
      *
      * @param responsible the responsible {@link User} to get all {@link KeyCategory} entities of
-     * @return a collection of all {@link KeyCategory} entities
-     * @throws NullPointerException if the given {@code responsible} is null
+     * @return a collection of all found {@link KeyCategory} entities
+     * @throws UserNotFoundException if the given {@code responsible} has not been found
      */
-    Set<KeyCategory> getAllByResponsible(User responsible) throws NullPointerException;
+    Set<KeyCategory> getAllByResponsible(User responsible) throws UserNotFoundException;
+
+    /**
+     * Gets all {@link Key} entities of the given {@link KeyCategory}.
+     *
+     * @param category the {@link KeyCategory} to get all {@link Key} entities of
+     * @return a collection of all found {@link KeyCategory} entities
+     * @throws KeyCategoryNotFoundException if the given {@code category} has not been found
+     */
+    Set<Key> getAllKeys(KeyCategory category) throws KeyCategoryNotFoundException;
 
     /**
      * Gets the {@link KeyCategory} with the given {@code id}.
      *
-     * @param id The ID of the {@link KeyCategory}
+     * @param id the ID of the {@link KeyCategory}
      * @return the {@link KeyCategory} with the given ID
-     * @throws NullPointerException if the given {@code id} is null
+     * @throws KeyCategoryNotFoundException if the {@link KeyCategory} entity with the given {@code id} has not been found
      */
-    KeyCategory getById(long id) throws NullPointerException;
+    KeyCategory getById(long id) throws KeyCategoryNotFoundException;
 
     /**
      * Gets the child {@link KeyCategory} entities of the given {@code category}.
      *
-     * @return a collection of child {@link KeyCategory} entities
-     * @throws NullPointerException if the given {@code category} is null
+     * @param category the {@link KeyCategory} to get all child entities of
+     * @return a collection all found child {@link KeyCategory} entities
+     * @throws KeyCategoryNotFoundException if the given {@code category} has not been found
      */
-    Set<KeyCategory> getChilds(KeyCategory category) throws NullPointerException;
+    Set<KeyCategory> getChilds(KeyCategory category) throws KeyCategoryNotFoundException;
 
     /**
      * Gets the {@link Key} entity with the given {@code id} if owned by the given {@link KeyCategory}.
@@ -107,14 +113,14 @@ public interface KeyCategoryService {
      * @param id the ID of the {@link Key} entity to get of the the given {@code category}
      * @param category the {@link KeyCategory} which owns the {@link Key} entity
      * @return the {@link Key} entity if owned by the given {@code category}
-     * @throws NullPointerException if the given {@code category} is null
+     * @throws KeyCategoryNotFoundException if the given {@code category} has not been found
      */
-    Optional<Key> getKey(long id, KeyCategory category) throws NullPointerException;
+    Optional<Key> getKey(long id, KeyCategory category) throws KeyCategoryNotFoundException;
 
     /**
      * Gets the parent {@link KeyCategory} of the given {@code category}.
      *
-     * @param category The {@link KeyCategory} to get the parent of
+     * @param category the {@link KeyCategory} to get the parent of
      * @return the parent {@link Optional<KeyCategory>}
      */
     Optional<KeyCategory> getParent(KeyCategory category);
@@ -122,12 +128,12 @@ public interface KeyCategoryService {
     /**
      * Checks if the given child {@link KeyCategory} is owned the given parent {@link KeyCategory} entity.
      *
-     * @param parentCategory the parent {@link KeyCategory} entity to check for the given child {@code category} for.
+     * @param parentCategory the parent {@link KeyCategory} entity to check for the given child {@code category} for
      * @param category the child {@link KeyCategory} to search
      * @return {@code true} if the given child {@code category} is owned by the given {@code parentCategory}, false otherwise
-     * @throws NullPointerException if the given {@code parentCategory} or {@code category} is null
+     * @throws KeyCategoryNotFoundException if the given {@code parentCategory} or {@code category} have not been found
      */
-    boolean hasChild(KeyCategory parentCategory, KeyCategory category) throws NullPointerException;
+    boolean hasChild(KeyCategory parentCategory, KeyCategory category) throws KeyCategoryNotFoundException;
 
     /**
      * Checks if the given {@link KeyCategory} directly owns the given {@link Key} entity.
@@ -135,26 +141,36 @@ public interface KeyCategoryService {
      * @param key the {@link Key} entity to look for
      * @param category the {@link KeyCategory} to search
      * @return {@code true} if the given {@code category} directly owns the given {@code key}, false otherwise
-     * @throws NullPointerException if the given {@code key} or {@code category} is null
+     * @throws KeyNotFoundException if the given {@code key} has not been found
+     * @throws KeyCategoryNotFoundException if the given {@code category} has not been found
      */
-    boolean hasKey(Key key, KeyCategory category) throws NullPointerException;
+    boolean hasKey(Key key, KeyCategory category) throws KeyNotFoundException, KeyCategoryNotFoundException;
 
     /**
      * Modifies the name of the given {@link KeyCategory}.
      *
-     * @param category The {@link KeyCategory} to modify the name of
-     * @param newName The new name for the given {@link KeyCategory}
-     * @throws NullPointerException if the given {@code category} is null
+     * @param category the {@link KeyCategory} to modify the name of
+     * @param newName the new name for the given {@link KeyCategory}
+     * @throws KeyCategoryNotFoundException if the given {@code category} has not been found
      * @throws IllegalArgumentException if the given new name is empty
      */
-    void modifyName(KeyCategory category, String newName) throws NullPointerException, IllegalArgumentException;
+    void modifyName(KeyCategory category, String newName) throws KeyCategoryNotFoundException, IllegalArgumentException;
 
     /**
      * Saves the given {@link KeyCategory} in the {@link KeyCategoryRepository}.
      *
-     * @param category The {@link KeyCategory} to save
+     * @param category the {@link KeyCategory} to save
      * @return the saved {@link KeyCategory}
      * @throws NullPointerException if the given {@code category} is null
      */
     KeyCategory save(KeyCategory category) throws NullPointerException;
+
+    /**
+     * Validates that the given {@link KeyCategory} entity exists in the {@link de.communicode.communikey.repository.KeyCategoryRepository}.
+     *
+     * @param keyCategoryId the ID of the {@link KeyCategory} entity to validate
+     * @return the validated {@link KeyCategory}
+     * @throws KeyCategoryNotFoundException if the {@link KeyCategory} with the given {@code keyCategoryId} has not been found
+     */
+    KeyCategory validate(long keyCategoryId) throws KeyCategoryNotFoundException;
 }
--- src/main/java/de/communicode/communikey/service/KeyService.java
+++ src/main/java/de/communicode/communikey/service/KeyService.java
@@ -7,66 +7,106 @@
 package de.communicode.communikey.service;
 
 import de.communicode.communikey.domain.Key;
+import de.communicode.communikey.domain.KeyCategory;
+import de.communicode.communikey.domain.User;
+import de.communicode.communikey.exception.KeyNotFoundException;
 
 import java.sql.Timestamp;
 import java.util.Set;
 
 /**
- * A service to interact with the {@link de.communicode.communikey.repository.KeyRepository}.
+ * A service to process {@link Key} entities.
  *
  * @author sgreb@communicode.de
  * @since 0.1.0
  */
 public interface KeyService {
+
+    /**
+     * Deletes the given {@link Key}.
+     *
+     * @param key the {@link Key} entity to delete
+     * @throws NullPointerException if the given {@code key} entity is null
+     */
+    void delete(Key key) throws NullPointerException;
+
     /**
      * Gets all {@link Key} entities of the {@link de.communicode.communikey.repository.KeyRepository}.
      *
      * @return a collection of all {@link Key} entities
      */
-    Set<Key> getAllKeys();
+    Set<Key> getAll();
+
+    /**
+     * Gets all {@link Key} entities with the given {@link KeyCategory}.
+     *
+     * @param keyCategory the {@link KeyCategory} from which all {@link Key} entities are to be found
+     * @return a collection of found {@link Key} entities
+     * @since 0.2.0
+     */
+    Set<Key> getAllByKeyCategory(KeyCategory keyCategory);
+
+    /**
+     * Gets all {@link Key} entities with the given creation {@code timestamp}.
+     *
+     * @param timestamp the creation {@link Timestamp} from which all {@link Key} are to be found
+     * @return a collection of found {@link Key} entities
+     * @since 0.2.0
+     */
+    Set<Key> getAllByCreationTimestamp(Timestamp timestamp);
+
+    /**
+     * Gets all {@link Key} entities created by the given {@link User}.
+     *
+     * @param creator the {@link User} entity to find all created {@link Key} entities of
+     * @return a collection of found {@link Key} entities
+     * @since 0.2.0
+     */
+    Set<Key> getAllByCreator(User creator);
+
+    /**
+     * Gets all {@link Key} entities with the given {@code value}.
+     *
+     * @param value the value from which all {@link Key} entity are to be found
+     * @return a collection of found {@link Key} entities
+     * @since 0.2.0
+     */
+    Set<Key> getAllByValue(String value);
 
     /**
      * Gets the {@link Key} with the given {@code id}.
      *
-     * @param id The ID of the {@link Key}
-     * @return the {@link Key} with the given ID
-     * @throws NullPointerException if the given {@code id} is invalid
+     * @param keyId the ID of the {@link Key} entity to get
+     * @return the {@link Key} entity with the given ID
+     * @throws de.communicode.communikey.exception.KeyNotFoundException if no {@link Key} with the given {@code keyId} has been found
      */
-    Key getKeyById(long id) throws NullPointerException;
-
-    /**
-     * Gets the first {@link Key} found with the given creation date.
-     *
-     * @param timestamp The {@link Timestamp} of a {@link Key} to find
-     * @return a {@link Key} with the given creation timestamp
-     * @throws NullPointerException if no key found with the given {@code timestamp}
-     */
-    Key getKeyByCreationDate(Timestamp timestamp) throws NullPointerException;
-
-    /**
-     * Deletes the given {@link Key}.
-     *
-     * @param key The {@link Key} to delete
-     * @throws NullPointerException if the given {@code key} is null
-     */
-    void deleteKey(Key key) throws NullPointerException;
+    Key getById(long keyId) throws KeyNotFoundException;
 
     /**
      * Modifies the value of the given {@link Key}.
      *
-     * @param key The {@link Key} to modify the value of
-     * @param newValue The new value for the given {@link Key}
-     * @throws NullPointerException if the given {@code key} is null
+     * @param key the {@link Key} entity to modify the value of
+     * @param newValue the new value for the given {@link Key} entity
+     * @throws NullPointerException if the given {@code key} entity is null
      * @throws IllegalArgumentException if the given new value is empty
      */
-    void modifyKeyValue(Key key, String newValue) throws NullPointerException, IllegalArgumentException;
+    void modifyValue(Key key, String newValue) throws NullPointerException, IllegalArgumentException;
 
     /**
      * Saves the given {@link Key} in the {@link de.communicode.communikey.repository.KeyRepository}.
      *
-     * @param key The {@link Key} to save
+     * @param key the {@link Key} entity to save
      * @return the saved {@link Key}
-     * @throws NullPointerException if the given {@code key} is null
+     * @throws NullPointerException if the given {@code key} entity is null
      */
-    Key saveKey(Key key) throws NullPointerException;
+    Key save(Key key) throws NullPointerException;
+
+    /**
+     * Validates that the given {@link Key} entity exists in the {@link de.communicode.communikey.repository.KeyRepository}.
+     *
+     * @param keyId the ID of the {@link Key} entity to validate
+     * @return the validated {@link Key}
+     * @throws KeyNotFoundException if the {@link Key} with the given {@code keyId} has not been found
+     */
+    Key validate(long keyId) throws KeyNotFoundException;
 }
