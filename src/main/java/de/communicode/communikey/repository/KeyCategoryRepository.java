ba8e73659247779618d166bf765a9fa55af9ff9e
Sven Greb <sgreb@communicode.de>
2016-11-10 15:53:05 +0100

    CCKEY-45 Update Key- and KeyCategory repositories

--- src/main/java/de/communicode/communikey/repository/KeyCategoryRepository.java
+++ src/main/java/de/communicode/communikey/repository/KeyCategoryRepository.java
@@ -8,57 +8,59 @@ package de.communicode.communikey.repository;
 
 import de.communicode.communikey.domain.KeyCategory;
 import de.communicode.communikey.domain.User;
+import de.communicode.communikey.exception.KeyCategoryNotFoundException;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Set;
 
 /**
- * The repository for {@link KeyCategory} entities.
+ * A repository for {@link KeyCategory} entities.
  *
  * @author sgreb@communicode.de
  * @since 0.2.0
  */
 @Repository
 public interface KeyCategoryRepository extends CrudRepository<KeyCategory, Long> {
+
     /**
-     * Finds and returns all {@link KeyCategory} entities created by the given {@link User}.
+     * Finds all {@link KeyCategory} entities created by the given {@link User}.
      *
-     * @param creator The {@link User} to get all created {@link KeyCategory} entities of
+     * @param creator the {@link User} to get all created {@link KeyCategory} entities of
      * @return a collection of all found {@link KeyCategory} entities
      */
     Set<KeyCategory> findAllByCreator(User creator);
 
     /**
-     * Finds and returns all {@link KeyCategory} entities with the given {@code name}.
+     * Finds all {@link KeyCategory} entities with the given {@code name}.
      *
-     * @param name The name of the {@link KeyCategory} entities to find
+     * @param name the name of the {@link KeyCategory} entities to find
      * @return a collection of all found {@link KeyCategory} entities
      */
     Set<KeyCategory> findAllByName(String name);
 
     /**
-     * Finds and returns all {@link KeyCategory} entities owned by the given parent {@link KeyCategory}.
+     * Finds all {@link KeyCategory} entities owned by the given parent {@link KeyCategory}.
      *
-     * @param keyCategory The parent {@link KeyCategory} to get all child {@link KeyCategory} entities of
+     * @param keyCategory the parent {@link KeyCategory} to get all child {@link KeyCategory} entities of
      * @return a collection of all found {@link KeyCategory} entities
      */
     Set<KeyCategory> findAllByParent(KeyCategory keyCategory);
 
     /**
-     * Finds and returns all {@link KeyCategory} entities the given {@link User} is responsible for.
+     * Finds  all {@link KeyCategory} entities the given {@link User} is responsible for.
      *
-     * @param responsible The responsible {@link User} to get all {@link KeyCategory} entities of
+     * @param responsible the responsible {@link User} to get all {@link KeyCategory} entities of
      * @return a collection of all found {@link KeyCategory} entities
      */
     Set<KeyCategory> findAllByResponsible(User responsible);
 
     /**
-     * Finds and returns the {@link KeyCategory} specified by the given {@code id}.
+     * Finds the {@link KeyCategory} with by the given {@code id}.
      *
-     * @param id The ID of the {@link KeyCategory} to find
-     * @return the {@link KeyCategory} with the given {@code id}
+     * @param id the ID of the {@link KeyCategory} to find
+     * @return the {@link KeyCategory} entity if found
+     * @throws KeyCategoryNotFoundException if no {@link KeyCategory} entity with the given {@code id} has been found
      */
-    KeyCategory findOneById(long id);
+    KeyCategory findOneById(long id) throws KeyCategoryNotFoundException;
 }
--- src/main/java/de/communicode/communikey/repository/KeyRepository.java
+++ src/main/java/de/communicode/communikey/repository/KeyRepository.java
@@ -7,32 +7,65 @@
 package de.communicode.communikey.repository;
 
 import de.communicode.communikey.domain.Key;
+import de.communicode.communikey.domain.KeyCategory;
+import de.communicode.communikey.domain.User;
+import de.communicode.communikey.exception.KeyNotFoundException;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import java.sql.Timestamp;
+import java.util.Set;
 
 /**
- * The repository for {@link Key} entities.
+ * A repository for {@link Key} entities.
  *
  * @author sgreb@communicode.de
  * @since 0.1.0
  */
 @Repository
 public interface KeyRepository extends CrudRepository<Key, Long> {
-    /**
-     * Finds and returns the {@link Key} specified by the given {@code id}.
-     *
-     * @param id The ID of the {@link Key} to find
-     * @return the {@link Key} with the given {@code id}
-     */
-    Key findOneById(long id);
 
     /**
-     * Finds and returns the first {@link Key} found with the given creation {@link Timestamp}.
+     * Finds all {@link Key} entities with the given {@link KeyCategory}.
      *
-     * @param creationTimestamp The creation {@link Timestamp} for a {@link Key} to find
-     * @return the {@link Key} with the given creation {@link Timestamp}
+     * @param category the {@link KeyCategory} from which all {@link Key} are to be found
+     * @return a collection of found {@link Key} entities
+     * @since 0.2.0
      */
-    Key findOneByCreationTimestamp(Timestamp creationTimestamp);
+    Set<Key> findAllByCategory(KeyCategory category);
+
+    /**
+     * Finds all {@link Key} entities with the given creation {@link Timestamp}.
+     *
+     * @param creationTimestamp the creation {@link Timestamp} from which all {@link Key} are to be found
+     * @return a collection of found {@link Key} entities
+     */
+    Set<Key> findAllByCreationTimestamp(Timestamp creationTimestamp);
+
+    /**
+     * Finds all {@link Key} entities created by the given {@link User}.
+     *
+     * @param creator the {@link User} to find all created {@link Key} entities of
+     * @return a collection of found {@link Key} entities
+     * @since 0.2.0
+     */
+    Set<Key> findAllByCreator(User creator);
+
+    /**
+     * Finds all {@link Key} entities with the given {@code value}.
+     *
+     * @param value the value from which all {@link Key} are to be found
+     * @return a collection of found {@link Key} entities
+     * @since 0.2.0
+     */
+    Set<Key> findAllByValue(String value);
+
+    /**
+     * Finds the {@link Key} entity with the given {@code id}.
+     *
+     * @param id the ID of the {@link Key} to find
+     * @return the {@link Key} entity if found
+     * @throws KeyNotFoundException if no {@link Key} entity with the given {@code id} has been found
+     */
+    Key findOneById(long id) throws KeyNotFoundException;
 }
