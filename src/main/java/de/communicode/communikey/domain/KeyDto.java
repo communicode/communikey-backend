1d0a92840daf8fd9f26dbca4e034050d5af207f1
Sven Greb <sgreb@communicode.de>
2016-11-10 15:47:44 +0100

    CCKEY-57 Implement DTO classes for Key- and KeyCategory entities

---
+++ src/main/java/de/communicode/communikey/domain/KeyDto.java
@@ -0,0 +1,67 @@
+/*
+ * Copyright (C) communicode AG - All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ * 2016
+ */
+package de.communicode.communikey.domain;
+
+import java.sql.Timestamp;
+
+/**
+ * A data transfer object for a {@link Key} entity.
+ *
+ * @author sgreb@communicode.de
+ * @since 0.2.0
+ */
+public class KeyDto {
+    private long id;
+
+    private long categoryId;
+
+    private long creatorId;
+
+    private Timestamp creationTimestamp;
+
+    private String value;
+
+    public long getCategoryId() {
+        return categoryId;
+    }
+
+    public Timestamp getCreationTimestamp() {
+        return creationTimestamp;
+    }
+
+    public long getCreatorId() {
+        return creatorId;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setCategoryId(Long categoryId) {
+        this.categoryId = categoryId;
+    }
+
+    public void setCreationTimestamp(Timestamp creationTimestamp) {
+        this.creationTimestamp = creationTimestamp;
+    }
+
+    public void setCreatorId(Long creatorId) {
+        this.creatorId = creatorId;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+}
