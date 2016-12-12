97d339289c6576041f473744fe0af54940091601
Sven Greb <sgreb@communicode.de>
2016-12-12 14:06:05 +0100

    CCKEY-60 Implement the global method security config

---
+++ src/main/java/de/communicode/communikey/config/MethodSecurityConfig.java
@@ -0,0 +1,29 @@
+/*
+ * Copyright (C) communicode AG - All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ * 2016
+ */
+package de.communicode.communikey.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.access.expression.method.MethodSecurityExpressionHandler;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.method.configuration.GlobalMethodSecurityConfiguration;
+import org.springframework.security.oauth2.provider.expression.OAuth2MethodSecurityExpressionHandler;
+
+/**
+ * Enables the global method security using the OAuth2 expression handler.
+ *
+ * @author sgreb@communicode.de
+ * @since 0.2.0
+ */
+@Configuration
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class MethodSecurityConfig extends GlobalMethodSecurityConfiguration {
+
+    @Override
+    protected MethodSecurityExpressionHandler createExpressionHandler() {
+        return new OAuth2MethodSecurityExpressionHandler();
+    }
+}
