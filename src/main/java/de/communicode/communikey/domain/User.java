6001588f6f731b1b4595d7c34fbfdfbbb262be95
Sven Greb <sgreb@communicode.de>
2016-12-13 11:09:29 +0100

    CCKEY-45 Fix return value of the User.getGroups() method

--- src/main/java/de/communicode/communikey/domain/User.java
+++ src/main/java/de/communicode/communikey/domain/User.java
@@ -124,7 +124,7 @@ public class User implements UserDetails {
     }
 
     public Set<UserGroup> getGroups() {
-        return new HashSet<>(groups);
+        return groups;
     }
 
     public long getId() {
