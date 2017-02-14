a5056e28102235f80c7da4bc49650794ad1e942c
Sven Greb <sgreb@communicode.de>
2017-02-14 14:58:17 +0100

    CCKEY-60 Update web security configurations

--- src/main/java/de/communicode/communikey/config/SecurityConfig.java
+++ src/main/java/de/communicode/communikey/config/SecurityConfig.java
@@ -2,22 +2,27 @@
  * Copyright (C) communicode AG - All Rights Reserved
  * Unauthorized copying of this file, via any medium is strictly prohibited
  * Proprietary and confidential
- * 2016
+ * 2017
  */
 package de.communicode.communikey.config;
 
 import static java.util.Objects.requireNonNull;
 
-import de.communicode.communikey.service.UserDetailsRestService;
+import org.springframework.beans.factory.BeanInitializationException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+
+import javax.annotation.PostConstruct;
 
 /**
  * Configures the security for user authentications and requests to the REST API.
@@ -31,16 +36,48 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-    private final UserDetailsRestService userDetailsService;
+    private final AuthenticationManagerBuilder authenticationManagerBuilder;
+    private final UserDetailsService userDetailsService;
+
+    public static final String APP_ID = "communikey";
+    public static final String EMAIL_REGEX = "^([^-_.@0-9]){1,}([a-z._-]{1,})@communicode\\.de$";
 
     @Autowired
-    public SecurityConfig(UserDetailsRestService userDetailsService) {
+    public SecurityConfig(AuthenticationManagerBuilder authenticationManagerBuilder, UserDetailsService userDetailsService) {
+        this.authenticationManagerBuilder = requireNonNull(authenticationManagerBuilder, "authenticationManagerBuilder must not be null!");
         this.userDetailsService = requireNonNull(userDetailsService, "userDetailsService must not be null!");
+    }
+
+    @PostConstruct
+    public void init() {
+        try {
+            authenticationManagerBuilder
+                .userDetailsService(userDetailsService)
+                .passwordEncoder(passwordEncoder());
+        } catch (Exception e) {
+            throw new BeanInitializationException("Security configuration initialization failed", e);
+        }
     }
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
+    }
+
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+        http
+            .httpBasic().realmName(APP_ID)
+        .and()
+            .sessionManagement()
+            .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+        .and()
+            .requestMatchers().antMatchers("/oauth/authorize")
+        .and()
+            .authorizeRequests()
+            .antMatchers("/oauth/authorize").authenticated()
+        .and()
+            .csrf().disable();
     }
 
     @Override
