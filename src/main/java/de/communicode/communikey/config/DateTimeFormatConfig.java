78db22928462e443f35efe30ab9be70f4945f6a4
Sven Greb <sgreb@communicode.de>
2017-02-14 14:54:30 +0100

    CCKEY-45 Configure global ISO based date format

---
+++ src/main/java/de/communicode/communikey/config/DateTimeFormatConfig.java
@@ -0,0 +1,23 @@
+/*
+ * Copyright (C) communicode AG - All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ * 2017
+ */
+package de.communicode.communikey.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.format.FormatterRegistry;
+import org.springframework.format.datetime.standard.DateTimeFormatterRegistrar;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+public class DateTimeFormatConfig extends WebMvcConfigurerAdapter {
+
+    @Override
+    public void addFormatters(FormatterRegistry registry) {
+        DateTimeFormatterRegistrar registrar = new DateTimeFormatterRegistrar();
+        registrar.setUseIsoFormat(true);
+        registrar.registerFormatters(registry);
+    }
+}
