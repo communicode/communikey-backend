882d6f239eaae8b5e64a05786e7fd175d34c092b
Sven Greb <sgreb@communicode.de>
2017-02-14 14:57:02 +0100

    CCKEY-60 Update OAuth2 configurations

--- src/main/java/de/communicode/communikey/config/OAuth2Config.java
+++ src/main/java/de/communicode/communikey/config/OAuth2Config.java
@@ -2,18 +2,18 @@
  * Copyright (C) communicode AG - All Rights Reserved
  * Unauthorized copying of this file, via any medium is strictly prohibited
  * Proprietary and confidential
- * 2016
+ * 2017
  */
 package de.communicode.communikey.config;
 
-import de.communicode.communikey.service.UserDetailsRestService;
+import de.communicode.communikey.security.AuthoritiesConstants;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
@@ -22,46 +22,94 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.R
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.token.DefaultAccessTokenConverter;
 import org.springframework.security.oauth2.provider.token.TokenStore;
 import org.springframework.security.oauth2.provider.token.store.JdbcTokenStore;
 
-import static de.communicode.communikey.CommunikeyApplication.APP_ID;
-import static de.communicode.communikey.type.PrivilegeType.READ;
-import static de.communicode.communikey.type.PrivilegeType.WRITE;
-import static de.communicode.communikey.type.RoleType.ROLE_ADMIN;
-import static de.communicode.communikey.type.RoleType.ROLE_USER;
+import javax.sql.DataSource;
+
+import static de.communicode.communikey.config.SecurityConfig.APP_ID;
+import static de.communicode.communikey.controller.RequestMappings.API;
+import static de.communicode.communikey.controller.RequestMappings.USERS;
+import static de.communicode.communikey.controller.RequestMappings.USERS_ACTIVATE;
+import static de.communicode.communikey.controller.RequestMappings.USERS_PASSWORD_RESET;
+import static de.communicode.communikey.controller.RequestMappings.USERS_REGISTER;
 import static java.util.Objects.requireNonNull;
 
 /**
- * Holds both configuration classes for the OAuth2 authorization- and resource server.
+ * Configures the OAuth2 authorization- and resource server.
  *
  * @author sgreb@communicode.de
  * @since 0.2.0
  */
+@Configuration
 public class OAuth2Config {
+
+    private final DataSource dataSource;
+
+    @Bean
+    public JdbcTokenStore tokenStore() {
+        return new JdbcTokenStore(dataSource);
+    }
+
+    @Autowired
+    public OAuth2Config(DataSource dataSource) {
+        this.dataSource =requireNonNull(dataSource, "dataSource must not be null!");
+    }
 
     /**
      * Configures the OAuth2 resource server.
+     *
+     * @author sgreb@communicode.de
+     * @since 0.2.0
      */
     @Configuration
     @EnableResourceServer
     protected static class ResourceServerConfiguration extends ResourceServerConfigurerAdapter {
 
+        private final TokenStore tokenStore;
+
+        @Autowired
+        public ResourceServerConfiguration(TokenStore tokenStore) {
+            this.tokenStore = requireNonNull(tokenStore, "tokenStore must not be null!");
+        }
+
+        @Bean
+        public DefaultAccessTokenConverter defaultAccessTokenConverter() {
+            DefaultAccessTokenConverter defaultAccessTokenConverter = new DefaultAccessTokenConverter();
+            defaultAccessTokenConverter.setIncludeGrantType(true);
+            return defaultAccessTokenConverter;
+        }
+
         @Override
         public void configure(ResourceServerSecurityConfigurer resources) {
-            resources.resourceId(APP_ID);
+            resources
+                .resourceId(APP_ID)
+                .tokenStore(tokenStore);
         }
 
         @Override
         public void configure(HttpSecurity http) throws Exception {
-            http.authorizeRequests().anyRequest().authenticated()
-                .and()
+            http
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+            .and()
+                .authorizeRequests()
+                .antMatchers(USERS + USERS_REGISTER).permitAll()
+                .antMatchers(USERS + USERS_ACTIVATE).permitAll()
+                .antMatchers(USERS + USERS_PASSWORD_RESET).permitAll()
+                .antMatchers("/oauth/authorize").permitAll()
+                .antMatchers(API + "/**").authenticated()
+            .and()
                 .csrf().disable();
         }
     }
 
     /**
      * Configures the OAuth2 authorization server.
+     *
+     * @author sgreb@communicode.de
+     * @since 0.2.0
      */
     @Configuration
     @EnableAuthorizationServer
@@ -70,54 +118,40 @@ public class OAuth2Config {
         @Qualifier("authenticationManagerBean")
         private final AuthenticationManager authenticationManager;
 
-        private UserDetailsRestService userDetailsRestService;
-        private final DataSourceConfig dataSourceConfig;
+        private final TokenStore tokenStore;
 
-        @Value("${cckey.security.oauth2.access_token_validity}")
-        private int access_token_validity;
-
-        @Value("${cckey.security.oauth2.refresh_token_validity}")
-        private int refresh_token_validity;
+        private final CommunikeyProperties communikeyProperties;
 
         @Autowired
-        public AuthorizationServerConfiguration(AuthenticationManager authenticationManager, DataSourceConfig dataSourceConfig,
-                                                UserDetailsRestService userDetailsRestService) {
+        public AuthorizationServerConfiguration(AuthenticationManager authenticationManager, TokenStore tokenStore, CommunikeyProperties communikeyProperties) {
             this.authenticationManager = requireNonNull(authenticationManager, "authenticationManager must not be null!");
-            this.dataSourceConfig = requireNonNull(dataSourceConfig, "dataSourceConfig must not be null!");
-            this.userDetailsRestService = requireNonNull(userDetailsRestService, "userDetailsRestService must not be null!");
+            this.tokenStore = requireNonNull(tokenStore, "tokenStore must not be null!");
+            this.communikeyProperties = requireNonNull(communikeyProperties, "communikeyProperties must not be null!");
         }
 
         @Override
         public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
             endpoints
-                .authenticationManager(authenticationManager)
-                .userDetailsService(userDetailsRestService)
-                .tokenStore(tokenStore());
+                .tokenStore(tokenStore)
+                .authenticationManager(authenticationManager);
         }
 
         @Override
         public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
-            clients.jdbc(dataSourceConfig.dataSource())
+            clients.inMemory()
                 .withClient(APP_ID)
-                .authorizedGrantTypes("password", "refresh_token")
-                .authorities(ROLE_USER.name(), ROLE_ADMIN.name())
-                .autoApprove(true)
+                .secret(communikeyProperties.getSecurity().getoAuth2().getSecret())
                 .resourceIds(APP_ID)
-                .scopes(READ.name().toLowerCase(), WRITE.name().toLowerCase())
-                .accessTokenValiditySeconds(access_token_validity)
-                .refreshTokenValiditySeconds(refresh_token_validity);
+                .authorizedGrantTypes("implicit")
+                .authorities(AuthoritiesConstants.ADMIN, AuthoritiesConstants.USER)
+                .scopes("read", "write")
+                .autoApprove(true)
+                .accessTokenValiditySeconds(communikeyProperties.getSecurity().getoAuth2().getAccessTokenValidity());
         }
 
         @Override
         public void configure(AuthorizationServerSecurityConfigurer oauthServer) throws Exception {
-            oauthServer
-                .realm(APP_ID);
+            oauthServer.realm(APP_ID);
         }
-
-        @Bean
-        public TokenStore tokenStore() {
-            return new JdbcTokenStore(dataSourceConfig.dataSource());
-        }
-
     }
 }
