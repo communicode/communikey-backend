dc0dcd06eabaa8853f1b4f45c77511265d6854f3
Sven Greb <sgreb@communicode.de>
2017-02-15 15:57:19 +0100

    CCKEY-45 Implement constant class for path variables

---
+++ src/main/java/de/communicode/communikey/controller/PathVariables.java
@@ -0,0 +1,22 @@
+/*
+ * Copyright (C) communicode AG - All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ * 2017
+ */
+package de.communicode.communikey.controller;
+
+/**
+ * Provides path variables constants.
+ *
+ * @author sgreb@communicode.de
+ * @since 0.2.0
+ */
+public class PathVariables {
+
+    private PathVariables() {}
+
+    public static final String USER_ACTIVATION_KEY = "activation_key";
+    public static final String USER_EMAIL = "email";
+    public static final String USER_LOGIN = "login";
+}
