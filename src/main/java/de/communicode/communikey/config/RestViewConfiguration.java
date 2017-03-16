e7b63088c347b978f50c428055498f625c7258f9
Sven Greb <sgreb@communicode.de>
2017-03-16 15:27:01 +0100

    CCKEY-85 Use predicates for comparision

--- src/main/java/de/communicode/communikey/config/RestViewConfiguration.java
+++ src/main/java/de/communicode/communikey/config/RestViewConfiguration.java
@@ -15,12 +15,14 @@ import org.springframework.http.MediaType;
 import org.springframework.http.converter.json.MappingJacksonValue;
 import org.springframework.http.server.ServerHttpRequest;
 import org.springframework.http.server.ServerHttpResponse;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.servlet.mvc.method.annotation.AbstractMappingJacksonResponseBodyAdvice;
 
-import java.util.Collection;
+import java.util.Optional;
+import java.util.function.Predicate;
 
 /**
  * Configuration for {@link JsonView} annotated entity attributes to allow {@link Authority} based JSON response (de)serialization.
@@ -34,16 +36,17 @@ public class RestViewConfiguration extends AbstractMappingJacksonResponseBodyAdv
     @Override
     protected void beforeBodyWriteInternal(MappingJacksonValue bodyContainer, MediaType contentType, MethodParameter returnType, ServerHttpRequest request,
                                            ServerHttpResponse response) {
+        Predicate<String> checkForAdmin = s -> s.equals(AuthoritiesConstants.ADMIN);
+        Predicate<String> checkForUser = s -> s.equals(AuthoritiesConstants.USER);
+        Predicate<String> check = checkForAdmin.or(checkForUser);
 
-        Class<?> viewClass = AuthoritiesRestView.User.class;
-
-        if (SecurityContextHolder.getContext().getAuthentication() != null && SecurityContextHolder.getContext().getAuthentication().getAuthorities() != null) {
-            Collection<? extends GrantedAuthority> authorities = SecurityContextHolder.getContext().getAuthentication().getAuthorities();
-
-            if (authorities.stream().anyMatch(o -> o.getAuthority().equals(AuthoritiesConstants.ADMIN))) {
-                viewClass = AuthoritiesRestView.Admin.class;
-            }
-        }
+        Class<?> viewClass =  Optional.ofNullable(SecurityContextHolder.getContext().getAuthentication())
+                .map(Authentication::getAuthorities)
+                .filter(authorities -> authorities.stream()
+                        .map(GrantedAuthority::getAuthority)
+                        .anyMatch(check))
+                .<Class<?>>map(isTrue -> AuthoritiesRestView.Admin.class)
+                .orElse(AuthoritiesRestView.User.class);
         bodyContainer.setSerializationView(viewClass);
     }
 }
