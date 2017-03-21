7a2106d8dd61734dae5c13d3633bb3f94f427be1
Sven Greb <sgreb@communicode.de>
2017-03-21 16:03:38 +0100

    CCKEY-83 Remove unused key category- and key repository methods

--- src/main/java/de/communicode/communikey/repository/KeyRepository.java
+++ src/main/java/de/communicode/communikey/repository/KeyRepository.java
@@ -10,8 +10,6 @@ import de.communicode.communikey.domain.Key;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Set;
-
 /**
  * A repository for {@link Key} entities.
  *
@@ -20,24 +18,4 @@ import java.util.Set;
  */
 @Repository
 public interface KeyRepository extends JpaRepository<Key, Long> {
-
-/**
-     * Finds all keys created by a user with the specified login.
-     *
-     * @param login the login of the user to find all created keys of
-     * @return a collection of keys
-     * @since 0.2.0
-     */
-    Set<Key> findAllByCreatedBy(String login);
-
-    /**
-     * Finds the key with the specified name.
-     *
-     * @param name the name of the key to find
-     * @return the key if found
-     * @since 0.2.0
-     */
-    Key findOneByName(String name);
-
-    Set<Key> findAllByCategoryIsNull();
 }
