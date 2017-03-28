80b19eb2f79b8d7c9ec0e59be3f186aa2051a5d4
Sven Greb <sgreb@communicode.de>
2017-03-28 15:58:56 +0200

    CCKEY-99 Design the category marker interface

---
+++ src/test/java/de/communicode/communikey/CommunikeyIntegrationTest.java
@@ -0,0 +1,16 @@
+/*
+ * Copyright (C) communicode AG - All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ * 2017
+ */
+package de.communicode.communikey;
+
+/**
+ * Category marker interface to identify integration tests.
+ *
+ * @author sgreb@communicode.de
+ * @since 0.3.0
+ */
+public interface CommunikeyIntegrationTest {
+}
