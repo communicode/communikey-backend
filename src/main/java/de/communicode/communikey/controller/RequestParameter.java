d59df09182619ed4bb68061bd4d4375e4c25beb1
Sven Greb <sgreb@communicode.de>
2017-03-29 11:06:13 +0200

    CCKEY-98 Implement class to provide request parameter for controllers

---
+++ src/main/java/de/communicode/communikey/controller/RequestParameter.java
@@ -0,0 +1,28 @@
+/*
+ * Copyright (C) communicode AG - All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ * 2017
+ */
+package de.communicode.communikey.controller;
+
+/**
+ * Provides request parameter constants.
+ *
+ * @author sgreb@communicode.de
+ * @since 0.3.0
+ */
+public class RequestParameter {
+
+    /**
+     * The request parameter for the {@value RequestMappings#API} endpoint to get the privilege status of the current user.
+     */
+    public static final String API_PRIVILEGED = "privileged";
+
+    /**
+     * The request parameter for the {@value RequestMappings#API} endpoint to get the version of the REST API.
+     */
+    public static final String API_VERSION = "version";
+
+    private RequestParameter() {}
+}
