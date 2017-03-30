0188c39d7074108a361b815a6e10629424456361
Sven Greb <sgreb@communicode.de>
2017-03-30 14:30:33 +0200

    CCKEY-83 Remove ununsed imports

--- src/main/java/de/communicode/communikey/CommunikeyApplication.java
+++ src/main/java/de/communicode/communikey/CommunikeyApplication.java
@@ -22,7 +22,6 @@ import org.springframework.core.env.Environment;
 
 import javax.annotation.PostConstruct;
 
-import java.net.InetAddress;
 import java.net.UnknownHostException;
 import java.util.Arrays;
 import java.util.Collection;
