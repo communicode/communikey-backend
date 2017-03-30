27bd923acbcf4a814ba450cdb11f81a1581ecd3e
Sven Greb <sgreb@communicode.de>
2017-03-30 14:24:59 +0200

    CCKEY-99 Implement more integration test for User REST API endpoints

--- src/test/java/de/communicode/communikey/api/UserApiIntegrationTest.java
+++ src/test/java/de/communicode/communikey/api/UserApiIntegrationTest.java
@@ -7,60 +7,85 @@
 package de.communicode.communikey.api;
 
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.not;
+import static org.hamcrest.core.IsCollectionContaining.hasItem;
+import static org.hamcrest.core.IsCollectionContaining.hasItems;
 import static org.hamcrest.core.IsEqual.equalTo;
 import static org.hamcrest.CoreMatchers.containsString;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import de.communicode.communikey.IntegrationBaseTest;
 import de.communicode.communikey.controller.RequestMappings;
 import de.communicode.communikey.domain.User;
+import de.communicode.communikey.security.AuthoritiesConstants;
 import de.communicode.communikey.service.payload.UserPayload;
 import io.restassured.http.ContentType;
+import io.restassured.response.Response;
 import org.junit.Test;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
 
 public class UserApiIntegrationTest extends IntegrationBaseTest {
 
-    @Test
-    public void testCreateUserAsAdminWithValidPayload() throws JsonProcessingException {
-        User newUser = new User();
-        newUser.setLogin("newuser");
-        newUser.setEmail(newUser.getLogin() + "@communicode.de");
-        newUser.setFirstName("firstname");
-        newUser.setLastName("lastname");
-        newUser.setActivated(true);
-        newUser.setPassword(passwordEncoder.encode(decodedUserPassword));
+    private String testUserLogin = "test";
+    private String testUserEmail = testUserLogin + "@communicode.de";
+    private User testUser = new User();
 
-        given()
+    @Test
+    public void testActivateUserAsAdmin() {
+        initializeTestUser(false);
+        Response response = given()
                 .auth().oauth2(adminUserOAuth2AccessToken)
                 .contentType(ContentType.JSON)
-                .body(new UserPayload(newUser))
+                .body(new UserPayload(testUser))
         .when()
                 .post(RequestMappings.USERS + RequestMappings.USERS_REGISTER)
         .then()
                 .statusCode(HttpStatus.CREATED.value())
-                .body("login", equalTo(newUser.getLogin()))
-                .body("email", equalTo(newUser.getEmail()))
-                .body("firstName", equalTo(newUser.getFirstName()))
-                .body("lastName", equalTo(newUser.getLastName()))
-                .body("activated", equalTo(newUser.isActivated()));
+                .extract().response();
+
+        given()
+                .auth().oauth2(adminUserOAuth2AccessToken)
+                .queryParam("activation_key", response.path("activationKey").toString())
+        .when()
+                .get(RequestMappings.USERS + RequestMappings.USERS_ACTIVATE)
+        .then()
+                .statusCode(HttpStatus.NO_CONTENT.value());
     }
 
     @Test
-    public void testCreateUserAsAdminWithInvalidPayload() throws JsonProcessingException {
-        User newUser = new User();
-        newUser.setLogin("   new__userwith123$invalid//characters-||##5   ");
-        newUser.setEmail(newUser.getLogin() + "@invalid.de");
-        newUser.setFirstName("firstname");
-        newUser.setLastName("lastname");
-        newUser.setActivated(true);
-        newUser.setPassword(passwordEncoder.encode(decodedUserPassword));
+    public void testCreateUserAsAdminWithValidPayload() {
+        initializeTestUser(true);
+        given()
+                .auth().oauth2(adminUserOAuth2AccessToken)
+                .contentType(ContentType.JSON)
+                .body(new UserPayload(testUser))
+        .when()
+                .post(RequestMappings.USERS + RequestMappings.USERS_REGISTER)
+        .then()
+                .statusCode(HttpStatus.CREATED.value())
+                .body("login", equalTo(testUser.getLogin()))
+                .body("email", equalTo(testUser.getEmail()))
+                .body("firstName", equalTo(testUser.getFirstName()))
+                .body("lastName", equalTo(testUser.getLastName()))
+                .body("activated", equalTo(testUser.isActivated()));
+    }
+
+    @Test
+    public void testCreateUserAsAdminWithInvalidPayload() {
+        initializeTestUser(true);
+        testUser.setLogin("   new__userwith123$invalid//characters-||##5   ");
+        testUser.setEmail(testUser.getLogin() + "@invalid.de");
 
         given()
                 .auth().oauth2(adminUserOAuth2AccessToken)
                 .contentType(ContentType.JSON)
-                .body(new UserPayload(newUser))
+                .body(new UserPayload(testUser))
         .when()
                 .post(RequestMappings.USERS + RequestMappings.USERS_REGISTER)
         .then()
@@ -69,33 +94,83 @@ public class UserApiIntegrationTest extends IntegrationBaseTest {
     }
 
     @Test
-    public void testActivateUserAsAdmin() throws JsonProcessingException {
-        User newUser = new User();
-        newUser.setLogin("newuser");
-        newUser.setEmail(newUser.getLogin() + "@communicode.de");
-        newUser.setFirstName("firstname");
-        newUser.setLastName("lastname");
-        newUser.setActivated(false);
-        newUser.setPassword(passwordEncoder.encode(decodedUserPassword));
-
+    public void testDeactivateUserAsAdmin() {
+        initializeTestUser(true);
         given()
                 .auth().oauth2(adminUserOAuth2AccessToken)
                 .contentType(ContentType.JSON)
-                .body(new UserPayload(newUser))
+                .body(new UserPayload(testUser))
         .when()
                 .post(RequestMappings.USERS + RequestMappings.USERS_REGISTER)
         .then()
-                .statusCode(HttpStatus.CREATED.value())
-                .body(containsString("email not a well-formed email address"));
+                .statusCode(HttpStatus.CREATED.value());
 
         given()
                 .auth().oauth2(adminUserOAuth2AccessToken)
-                .contentType(ContentType.JSON)
-                .queryParam("activation_key", newUser.getActivationKey())
+                .queryParam("login", testUser.getLogin())
         .when()
-                .post(RequestMappings.USERS + RequestMappings.USERS_ACTIVATE)
+                .get(RequestMappings.USERS + RequestMappings.USERS_DEACTIVATE)
         .then()
                 .statusCode(HttpStatus.NO_CONTENT.value());
+
+        given()
+                .auth().oauth2(adminUserOAuth2AccessToken)
+                .pathParam("login", testUser.getLogin())
+        .when()
+                .get(RequestMappings.USERS + RequestMappings.USERS_LOGIN)
+        .then()
+                .statusCode(HttpStatus.OK.value())
+                .body("activated", equalTo(false));
+    }
+
+    @Test
+    public void testDeleteUserAsAdmin() {
+        initializeTestUser(true);
+        given()
+                .auth().oauth2(adminUserOAuth2AccessToken)
+                .contentType(ContentType.JSON)
+                .body(new UserPayload(testUser))
+        .when()
+                .post(RequestMappings.USERS + RequestMappings.USERS_REGISTER)
+        .then()
+                .statusCode(HttpStatus.CREATED.value());
+
+        given()
+                .auth().oauth2(adminUserOAuth2AccessToken)
+                .pathParam("login", testUser.getLogin())
+        .when()
+                .delete(RequestMappings.USERS + RequestMappings.USERS_LOGIN)
+        .then()
+                .statusCode(HttpStatus.NO_CONTENT.value());
+
+        given()
+                .auth().oauth2(adminUserOAuth2AccessToken)
+                .pathParam("login", testUser.getLogin())
+        .when()
+                .get(RequestMappings.USERS + RequestMappings.USERS_LOGIN)
+        .then()
+                .statusCode(HttpStatus.NOT_FOUND.value());
+    }
+
+    @Test
+    public void testGetOneUserAsAdmin() {
+        initializeTestUser(true);
+        given()
+                .auth().oauth2(adminUserOAuth2AccessToken)
+                .contentType(ContentType.JSON)
+                .body(new UserPayload(testUser))
+        .when()
+                .post(RequestMappings.USERS + RequestMappings.USERS_REGISTER)
+        .then()
+                .statusCode(HttpStatus.CREATED.value());
+
+        given()
+                .auth().oauth2(adminUserOAuth2AccessToken)
+                .pathParam("login", testUser.getLogin())
+        .when()
+                .get(RequestMappings.USERS + RequestMappings.USERS_LOGIN)
+        .then()
+                .statusCode(HttpStatus.OK.value());
     }
 
     @Test
@@ -116,5 +191,148 @@ public class UserApiIntegrationTest extends IntegrationBaseTest {
                 .get(RequestMappings.USERS)
         .then()
                 .statusCode(HttpStatus.FORBIDDEN.value());
+    }
+
+    @Test
+    public void testGetPasswordResetKeyAsUser() {
+        initializeTestUser(true);
+        given()
+                .auth().oauth2(adminUserOAuth2AccessToken)
+                .contentType(ContentType.JSON)
+                .body(new UserPayload(testUser))
+        .when()
+                .post(RequestMappings.USERS + RequestMappings.USERS_REGISTER)
+        .then()
+                .statusCode(HttpStatus.CREATED.value());
+
+        given()
+                .auth().oauth2(userOAuth2AccessToken)
+                .queryParam("email", testUser.getEmail())
+        .when()
+                .get(RequestMappings.USERS + RequestMappings.USERS_PASSWORD_RESET)
+        .then()
+                .statusCode(HttpStatus.OK.value());
+    }
+
+    @Test
+    public void testPasswordResetAsUser() {
+        initializeTestUser(true);
+        Map<String, String> userPasswordResetPayload = new HashMap<>();
+        userPasswordResetPayload.put("password", "newPassword");
+
+        given()
+                .auth().oauth2(adminUserOAuth2AccessToken)
+                .contentType(ContentType.JSON)
+                .body(new UserPayload(testUser))
+        .when()
+                .post(RequestMappings.USERS + RequestMappings.USERS_REGISTER)
+        .then()
+                .statusCode(HttpStatus.CREATED.value());
+
+        Response resetKeyResponse = given()
+                .auth().oauth2(userOAuth2AccessToken)
+                .queryParam("email", testUser.getEmail())
+        .when()
+                .get(RequestMappings.USERS + RequestMappings.USERS_PASSWORD_RESET)
+        .then()
+                .statusCode(HttpStatus.OK.value())
+                .extract().response();
+
+        userPasswordResetPayload.put("resetKey", resetKeyResponse.path("resetKey"));
+        given()
+                .auth().oauth2(userOAuth2AccessToken)
+                .contentType(ContentType.JSON)
+                .body(userPasswordResetPayload)
+        .when()
+                .post(RequestMappings.USERS + RequestMappings.USERS_PASSWORD_RESET)
+        .then()
+                .statusCode(HttpStatus.NO_CONTENT.value());
+    }
+
+    @Test
+    public void testUpdateAsAdmin() {
+        initializeTestUser(true);
+        given()
+                .auth().oauth2(adminUserOAuth2AccessToken)
+                .contentType(ContentType.JSON)
+                .body(new UserPayload(testUser))
+        .when()
+                .post(RequestMappings.USERS + RequestMappings.USERS_REGISTER)
+        .then()
+                .statusCode(HttpStatus.CREATED.value());
+
+        testUser.setEmail("newName@communicode.de");
+        given()
+                .auth().oauth2(adminUserOAuth2AccessToken)
+                .contentType(ContentType.JSON)
+                .body(new UserPayload(testUser))
+                .pathParam("login", testUser.getLogin())
+        .when()
+                .put(RequestMappings.USERS + RequestMappings.USERS_LOGIN)
+        .then()
+                .statusCode(HttpStatus.OK.value())
+                .body("login", equalTo("newName".toLowerCase()))
+                .body("email", equalTo(testUser.getEmail()))
+                .body("activated", equalTo(false));
+    }
+
+    @Test
+    public void testUpdateAuthoritiesAsAdmin() {
+        initializeTestUser(true);
+        Set<String> authorities = new HashSet<>();
+        authorities.add(AuthoritiesConstants.ADMIN);
+        authorities.add(AuthoritiesConstants.USER);
+
+        given()
+                .auth().oauth2(adminUserOAuth2AccessToken)
+                .contentType(ContentType.JSON)
+                .body(new UserPayload(testUser))
+        .when()
+                .post(RequestMappings.USERS + RequestMappings.USERS_REGISTER)
+        .then()
+                .statusCode(HttpStatus.CREATED.value())
+                .body("authorities.name", hasItem(containsString(AuthoritiesConstants.USER)))
+                .body("authorities.name", hasItem(not(containsString(AuthoritiesConstants.ADMIN))))
+                .root("authorities")
+                .body("size()", equalTo(1));
+        String invalidatedTestUserOAuth2AccessToken = generateOAuth2ImplicitGrantTypeFlowAccessToken(testUser.getLogin(), testUser.getPassword());
+
+        given()
+                .auth().oauth2(adminUserOAuth2AccessToken)
+                .contentType(ContentType.JSON)
+                .body(authorities)
+                .pathParam("login", testUser.getLogin())
+        .when()
+                .put(RequestMappings.USERS + RequestMappings.USER_AUTHORITIES)
+        .then()
+                .statusCode(HttpStatus.OK.value())
+                .body("email", equalTo(testUser.getEmail()))
+                .body("activated", equalTo(true))
+                .body("authorities.name", hasItem(containsString(AuthoritiesConstants.USER)))
+                .body("authorities.name", hasItem(containsString(AuthoritiesConstants.ADMIN)))
+                .root("authorities")
+                .body("size()", equalTo(2));
+
+        given()
+                .auth().oauth2(invalidatedTestUserOAuth2AccessToken)
+        .when()
+                .get(RequestMappings.KEY_CATEGORIES)
+        .then()
+                .statusCode(HttpStatus.UNAUTHORIZED.value())
+                .body(containsString("Invalid access token"));
+    }
+
+    /**
+     * Initializes the test user entity.
+     *
+     * @param activated the activation status of the test user
+     */
+    private void initializeTestUser(boolean activated) {
+        testUser.setLogin(testUserLogin);
+        testUser.setEmail(testUserEmail);
+        testUser.setFirstName(fairy.person().getFirstName());
+        testUser.setLastName(fairy.person().getLastName());
+        testUser.setActivated(activated);
+        testUser.setPassword(passwordEncoder.encode(decodedUserPassword));
     }
 }
