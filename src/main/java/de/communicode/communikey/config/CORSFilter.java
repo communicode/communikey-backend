38b3c7ea1c8a9cd639300f777a612bf74981c211
Sven Greb <sgreb@communicode.de>
2017-03-31 15:44:20 +0200

    CCKEY-83 Implement CORS filter
    Necessary to fix preflight requests of the UI (frontend).

---
+++ src/main/java/de/communicode/communikey/config/CORSFilter.java
@@ -0,0 +1,54 @@
+/*
+ * Copyright (C) communicode AG - All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ * 2017
+ */
+package de.communicode.communikey.config;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CORSFilter implements Filter {
+
+    private static final Logger log = LogManager.getLogger();
+
+    public CORSFilter() {
+        log.debug("Initialize CORS filter");
+    }
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {}
+
+    @Override
+    public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
+
+        HttpServletRequest request = (HttpServletRequest) req;
+        HttpServletResponse response = (HttpServletResponse) res;
+
+        response.setHeader("Access-Control-Allow-Origin", request.getHeader("Origin"));
+        response.setHeader("Access-Control-Allow-Credentials", "true");
+        response.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE, OPTIONS");
+        response.setHeader("Access-Control-Max-Age", "-1");
+        response.setHeader("Access-Control-Allow-Headers", "Content-Type, Accept, X-Requested-With, Authorization");
+        response.setHeader("Access-Control-Expose-Headers", "Location");
+
+        chain.doFilter(req, res);
+    }
+
+    @Override
+    public void destroy() {}
+}
