78c191654e93c1bf4eb294e7260d1acdb335f3ca
Dennis von der Bey <dvonderbey@communicode.de>
2017-07-26 13:43:43 +0200

    CCKEY-182 Changed asterisk import back

--- src/main/java/de/communicode/communikey/domain/Key.java
+++ src/main/java/de/communicode/communikey/domain/Key.java
@@ -13,7 +13,15 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.ObjectIdGenerators;
 import org.hibernate.validator.constraints.NotBlank;
 
-import javax.persistence.*;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GenerationType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.persistence.Lob;
 import javax.validation.constraints.NotNull;
 
 import java.io.Serializable;
