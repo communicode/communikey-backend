298e27b5a3d651d51679e8736708a8e81aacd3b1
Dennis von der Bey <dvonderbey@communicode.de>
2017-08-22 09:26:34 +0200

    CCKEY-237 Changed comment back

--- src/main/java/de/communicode/communikey/service/payload/UserPasswordResetPayload.java
+++ src/main/java/de/communicode/communikey/service/payload/UserPasswordResetPayload.java
@@ -12,9 +12,8 @@ import org.hibernate.validator.constraints.NotBlank;
 /**
  * A payload object to reset the password of a {@link User} with a specified reset token.
  *
- * @author dvonderbey@communicode.de
- * @author lleifermann@communicode.de
- * @since 0.15.0
+ * @author sgreb@communicode.de
+ * @since 0.2.0
  */
 public class UserPasswordResetPayload {
 
